dca9a033edea3ba1fe074c19f569f0e12ae6448c
ibrohimbekbegmamatov <itshool001@gmail.com>
2024-07-10 18:03:49 +0500

    swagger changed

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from .views import UsersViewSet
-
 urlpatterns = [
     # user Admin
     path('get/', UsersViewSet.as_view({'get': 'get_all'})),
-    path('delete/<int:id>/', UsersViewSet.as_view({'put': 'delete'})),
+    path('delete/<int:id>/', UsersViewSet.as_view({'delete': 'delete'})),
     path('get_id/<int:id>/', UsersViewSet.as_view({'get': 'get_by_id'})),
 ]
--- main/views.py
+++ main/views.py
@@ -10,31 +10,19 @@ from drf_yasg import openapi
 
 
 class UsersViewSet(ViewSet):
-    @swagger_auto_schema(
-        operation_description="Get all user",
-        operation_summary="Get all user",
-        request_body=openapi.Schema(
-            type=openapi.TYPE_OBJECT,
-            properties={
-                "token": openapi.Schema(type=openapi.TYPE_STRING, description="Token")
-            },
-            requiered=["token"]
-        ),
-        responses={200: UserSerializer(many=True)},
-        tags=['Admin']
-    )
+
     def check_token(self, token):
         response = requests.post('http://134.122.76.27:8114/api/v1/check/', data={'token': token})
         if response.status_code != 200:
             raise ValidationError({'error': 'Invalid token'})
 
+    @swagger_auto_schema(
+        operation_description="Get all user",
+        operation_summary="Get all user",
+        responses={200: UserSerializer(many=True)},
+        tags=['Admin']
+    )
     def get_all(self, request, *args, **kwargs):
-        self.check_token(request.data.get('token'))
-        access_token = request.headers.get('Authorization')
-        response_auth = requests.get(f"{settings.USER_MANAGEMENT_SERVICE_URL}/api/v1/auth/me/",
-                                     headers={'Authorization': access_token})
-        if response_auth.status_code != 200:
-            return Response({'error': 'Not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
         response = requests.get(f"{settings.USER_MANAGEMENT_SERVICE_URL}/getinfo/")
         if response.status_code == 200:
             return Response(response.json(), status=response.status_code)
@@ -47,8 +35,8 @@ class UsersViewSet(ViewSet):
         request_body=openapi.Schema(
             type=openapi.TYPE_OBJECT,
             properties={
-                "token": openapi.Schema(type=openapi.TYPE_STRING, description="Token"),
-                "user_id": openapi.Schema(type=openapi.TYPE_INTEGER, description="User_id")
+                "token": openapi.Schema(type=openapi.TYPE_STRING, title="Token"),
+                "user_id": openapi.Schema(type=openapi.TYPE_INTEGER, title="User_id")
             },
             requiered=["token", "user_id"]
         ),
@@ -64,7 +52,7 @@ class UsersViewSet(ViewSet):
             return Response({'error': 'Not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
 
         if not id:
-            return Response({'error': 'user id is required'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'user not found'}, status=status.HTTP_404_NOT_FOUND)
 
         response = requests.post(f"{settings.USER_MANAGEMENT_SERVICE_URL}/delete_user/{id}/")
         if response.status_code == 200:
@@ -73,32 +61,25 @@ class UsersViewSet(ViewSet):
             return Response({'error': 'Failed to delete user'}, status=status.HTTP_400_BAD_REQUEST)
 
     @swagger_auto_schema(
-        operation_description="Get by id",
-        operation_summary="Get by id",
-        request_body=openapi.Schema(
-            type=openapi.TYPE_OBJECT,
-            properties={
-                "token": openapi.Schema(type=openapi.TYPE_STRING, description="Token")
-            },
-            requiered=["token"]
-        ),
-        responses={200: UserSerializer()},
+        operation_description="Get by  user id",
+        operation_summary="Get by user id",
+        manual_parameters=[
+            openapi.Parameter('id', type=openapi.TYPE_INTEGER, description='get user by id',
+                              in_=openapi.IN_QUERY,
+                              required=True),
+        ],
+        responses={
+            200: UserSerializer(),
+            404: "Not found"
+        },
         tags=['Admin']
     )
     def get_by_id(self, request, id, *args, **kwargs):
-        self.check_token(request.data.get('token'))
-        access_token = request.headers.get('Authorization')
-        response_auth = requests.get(f"{settings.USER_MANAGEMENT_SERVICE_URL}/api/v1/auth/me/",
-                                     headers={'Authorization': access_token})
-        if response_auth.status_code != 200:
-            return Response({'error': 'Not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
-
         if not id:
-            return Response({'error': 'user_id is required'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'user not found'}, status=status.HTTP_404_NOT_FOUND)
 
         response = requests.get(f"{settings.USER_MANAGEMENT_SERVICE_URL}/user_details/{id}", )
         if response.status_code == 200:
             return Response(response.json(), status=response.status_code)
         else:
-            return Response({'error': 'Failed to retrieve user details'}, status=status.HTTP_400_BAD_REQUEST)
-
+            return Response({'error': 'Failed to get user details'}, status=status.HTTP_404_NOT_FOUND)
