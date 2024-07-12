6f76420671894233ceb4bd94f0a079f1d5dbc7ef
ibrohimbekbegmamatov <itshool001@gmail.com>
2024-07-12 19:46:35 +0500

    delete crud changed

--- main/urls.py
+++ main/urls.py
@@ -4,11 +4,11 @@ from .views import UsersViewSet, PostsViewSet
 urlpatterns = [
     # user admin
     path('get/users/', UsersViewSet.as_view({'get': 'get_all'})),
-    path('delete/user/<int:id>/', UsersViewSet.as_view({'get': 'delete'})),
+    path('delete/user/<int:id>/', UsersViewSet.as_view({'delete': 'delete'})),
     path('get/user/<int:id>/', UsersViewSet.as_view({'get': 'get_by_id'})),
     # post admin
     path('posts/', PostsViewSet.as_view({'get': 'get_all'})),
     path('post/<int:id>/', PostsViewSet.as_view({'get': 'get_by_id'})),
-    path('delete/post/<int:id>/', PostsViewSet.as_view({'get': 'delete_post'}))
+    path('delete/post/<int:id>/', PostsViewSet.as_view({'delete': 'delete_post'}))
 
 ]
